10d178ef7a45b0c38d151d59b8adf55b11f22a88
Vladislav Kapicyn <goodsnasey@gmail.com>
2018-10-31 14:01:36 +0300

    правки

--- bin/presenters/regPage.js
+++ bin/presenters/regPage.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const router = express.Router();
-const stakeholder = require('./../models/stakeholder').stakeholder;
+const stakeholder = require('./../models/stakeholder').stakeholderModel;
 const toHash = require('md5')
 
-class Main {
+class Registration {
     static getPage(req, res, next) {
         res.render('regPage.html')
     
     }
-    static reg(req,res,next){
-        console.log(req.body.login)
-        async function fun(){
-        if( !req.body.login || !req.body.password){console.log(req.body);res.json({'error':'Все поля должны быть заполнены'});return;}
-        let accountlogin = await stakeholder.findOne({"login":req.body.login});
-        console.log(accountlogin)
-        if (accountlogin != null) res.json({'error':'логин занят'})
-        else {
-            let account = new stakeholder({"login":req.body.login,"password":toHash(req.body.password),"state":0});
-            account.save();
-            res.json('аккаунт создан');
-            res.render('logPage.html')
-        }
-    }
-    fun();
-    }
-    
-    }
 
-router.get('/', Main.getPage);
-router.post('/',Main.reg);
+    //async можно прописывать прям тут
+    static async reg(req,res,next){
+        let err;
+        if( !req.body.login || !req.body.password) {
+            err = 'Все поля должны быть заполнены'; 
+        }
+
+        //проверка на уникальность логина должна быть на уровне схемы данных, если такой логин есть - выкинет ошибку на сохранении
+        try {
+            let account = new stakeholderModel({
+                'login': req.body.login,
+                'password': toHash(req.body.password),
+                'state': 0
+            });
+            account.save();
+        } catch (e) {
+            err = 'логин занят';
+        }
+        
+        res.render('logPage.html', {account: err || account});
+    }    
+}
+
+router.get('/', Registration.getPage);
+router.post('/', Registration.reg);
 
 module.exports.router = router;
